91f22ea658a33246e67cac813a9757dd8899d7be
biyonik <ahmet.altun60@gmail.com>
2024-11-25 19:42:16 +0300

    Initialised spring boot project

---
+++ securedoc/src/test/java/com/ahmetaltun/securedoc/SecuredocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmetaltun.securedoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
